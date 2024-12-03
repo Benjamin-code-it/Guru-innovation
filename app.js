eab0a400e572b2917229991c56a97c1c635f494a
Levi <ohazulikelevi5384@gmail.com>
2024-12-03 16:25:16 +0100

    updated

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const imgQr = document.querySelector('.imgQr');
-
 const imgQrCode= document.getElementById('imgQrCode');
 const qrbtn = document.querySelector('.qrBtn')
   
@@ -12,4 +11,11 @@ qrbtn.addEventListener('click', () => {
     }
     const value = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= ${qrText}`
     imgQrCode.setAttribute('src',value)
+    qrbtn.innerText = 'Scan code above'
+})
+
+
+const qrText = document.querySelector('.qrText')
+qrText.addEventListener('input',() => {
+    qrbtn.innerText = 'genarate Qr code'
 })
